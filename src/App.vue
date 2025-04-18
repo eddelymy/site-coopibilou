f8e95578b44a1790044b65f06a27f68c9d51edc1
Amina EDDELYMY <a.eddelymy@azura-group.com>
2025-04-18 11:14:46 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <GlobaleComponent/>
+</template>
+
+<script setup>
+import GlobaleComponent from './components/common/GlobaleComponent.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/common/GlobaleComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full h-screen flex flex-col">
+    <HeaderComponent/>
+    <div class="h-full w-full overflow-auto">
+      <RouterView />
+    </div>
+    
+  </div>
+
+</template>
+
+<script setup >
+import HeaderComponent from './HeaderComponent.vue';
+</script>
